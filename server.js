d1182f7c38f4816f1d1e8f5a0c52d69915747613
jeremyswann <mail@jeremyswann.com>
2018-07-26 10:58:10 +1000

    basic express setup

--- server.js
+++ server.js
@@ -3,8 +3,22 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello, Express!')
+	res.send({
+		name: 'Jeremy',
+		age: '29',
+		likes: ['Drawing', 'Guitar'],
+		pages: ['/about'],
+	})
+})
+
+app.get('/about', (req, res) => {
+	res.send('<h1>About Page</h1>')
+})
+
+app.get('/bad', (req, res) => {
+	res.send({
+		error_message: 'Unable to handle request.',
+	})
 })
 
 app.listen(3000)
-
